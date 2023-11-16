2fd771f9fb7b3624a59add53696ae918f9561695
Lieven <lieven.vaneeckhaute@gmail.com>
2023-11-16 21:49:36 +0100

    first draft.

---
+++ src/main/java/org/example/DHCPServer.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.net.*;
+
+public class DHCPServer {
+    private static final int DHCP_SERVER_PORT = 1067;
+    private static final int DHCP_CLIENT_PORT = 68;
+    private static final int DHCP_PACKET_SIZE = 548;
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket(DHCP_SERVER_PORT);
+            byte[] receiveData = new byte[DHCP_PACKET_SIZE];
+
+            System.out.println("DHCP server running...");
+
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+                /*
+                // Process received DHCP packet
+                DHCP_Packet requestPacket = processReceivedPacket(receivePacket.getData());
+
+                // Generate DHCP response packet
+                DHCP_Packet responsePacket = generateResponsePacket(requestPacket);
+
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+                DatagramPacket sendPacket = new DatagramPacket(
+                        serializePacket(responsePacket), serializePacket(responsePacket).length,
+                        clientAddress, clientPort);
+
+                // Send DHCP response to the client
+                socket.send(sendPacket);
+                */
+                // Handle DHCP lease allocation, IP assignment, etc.
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
